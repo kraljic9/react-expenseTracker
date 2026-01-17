a22e26238801b05657e19218b83c7138719b1b4a
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 16:38:00 +0100

    Displayed expenses in list items

--- expense-tracker/src/ExpenseTracker.jsx
+++ expense-tracker/src/ExpenseTracker.jsx
@@ -4,6 +4,19 @@ import { ExpenseContext } from "./ExpenseContext";
 function ExpenseTracker() {
   const { expenses, setExpenses, newExpense, setNewExpense } =
     useContext(ExpenseContext);
+
+  return (
+    <>
+      {expenses.map((expens) => (
+        <li key={expens.id}>
+          <span>{expens.text} </span>
+          <span>{expens.amount}$ </span>
+          <span>{expens.category} </span>
+          <span>{expens.date} </span>
+        </li>
+      ))}
+    </>
+  );
 }
 
 export default ExpenseTracker;
