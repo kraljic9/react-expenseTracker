83673b3fc5c14b79098daf53656d4c2b7f78e521
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 16:46:45 +0100

    Wrapped expense list in a wrapper div added input fields for new expenses

--- expense-tracker/src/ExpenseTracker.jsx
+++ expense-tracker/src/ExpenseTracker.jsx
@@ -7,14 +7,55 @@ function ExpenseTracker() {
 
   return (
     <>
-      {expenses.map((expens) => (
-        <li key={expens.id}>
-          <span>{expens.text} </span>
-          <span>{expens.amount}$ </span>
-          <span>{expens.category} </span>
-          <span>{expens.date} </span>
-        </li>
-      ))}
+      <h1>Expense Tracker App</h1>
+
+      <div className="input-wrapper">
+        <label htmlFor="" className="expense-label">
+          Expense Text
+        </label>
+        <br />
+        <input type="text" className="expense-input" />
+        <br />
+
+        <label htmlFor="" className="expense-label">
+          Expense Amount
+        </label>
+        <br />
+        <input type="number" className="expense-input" />
+        <br />
+
+        <label htmlFor="" className="expense-label">
+          Expense Category
+        </label>
+        <br />
+        <select name="" id="" className="expense-select">
+          <option value="">---Chose a category---</option>
+          <option value="clothes">Clothes</option>
+          <option value="groceries">Groceries</option>
+          <option value="bills">Bills</option>
+          <option value="eating out">Eating out</option>
+        </select>
+        <br />
+
+        <label htmlFor="" className="expense-label">
+          Expense Date
+        </label>
+        <br />
+        <input type="date" className="expense-select-date" />
+      </div>
+
+      <div className="expenses-wrapper">
+        <ul className="expenses-list">
+          {expenses.map((expens) => (
+            <li className="expense-li" key={expens.id}>
+              <span className="expense-txt">{expens.text} </span>
+              <span className="expense-amount">{expens.amount}$ </span>
+              <span className="expense-category">{expens.category} </span>
+              <span className="expense-date">{expens.date} </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
