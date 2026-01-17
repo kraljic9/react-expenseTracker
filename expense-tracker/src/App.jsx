a8b2da71f7bee8fe058349b707f1df53b61dd224
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 16:26:53 +0100

    Wrapped expense tracker componant in context provider componant

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-
+import ContextProvider from "./ContextProvider";
+import ExpenseTracker from "./ExpenseTracker";
 /*
   Expense tracker app to-do's:
 
-  1. Add state to track expenses
-  2. Add state for newExpense to add to expenses
-  2. Add Expense Context
+  1. Add state to track expenses OK!
+  2. Add state for newExpense to add to expenses OK!
+  2. Add Expense Context OK!
   3. Add input for text, amount, category and date
   6. Add expense, delete expense, edit expense functions
   4. Add sum amount for each catogry
@@ -14,7 +15,11 @@ import "./App.css";
 */
 
 function App() {
-  return;
+  return (
+    <ContextProvider>
+      <ExpenseTracker />
+    </ContextProvider>
+  );
 }
 
 export default App;
