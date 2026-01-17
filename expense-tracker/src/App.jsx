95a340cf96efa7fe860cdaa0612fd3fe242871db
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 15:57:48 +0100

    Prepared files needed for projects

---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+/*
+  Expense tracker app to-do's:
+
+  1. Add state to track expenses
+  2. Add Expense Context
+  3. Add input for text, amount, category and date
+  6. Add expense, delete expense, edit expense
+  4. Add sum amount for each catogry
+  5. Display different list depending on month
+  6. Persistance through local storage
+*/
+
+function App() {
+  return;
+}
+
+export default App;
