9c1df961bb23198047d2acacbb09706f7b5d98f4
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 16:20:31 +0100

    Added more comments to improve project blueprint

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -4,9 +4,10 @@ import "./App.css";
   Expense tracker app to-do's:
 
   1. Add state to track expenses
+  2. Add state for newExpense to add to expenses
   2. Add Expense Context
   3. Add input for text, amount, category and date
-  6. Add expense, delete expense, edit expense
+  6. Add expense, delete expense, edit expense functions
   4. Add sum amount for each catogry
   5. Display different list depending on month
   6. Persistance through local storage
