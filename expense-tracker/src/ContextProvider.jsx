747b1a71945c81d11cd006b6422ff77c93bda4a4
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-01-17 16:21:06 +0100

    Added state for expenses list and new expense object

--- expense-tracker/src/ContextProvider.jsx
+++ expense-tracker/src/ContextProvider.jsx
@@ -1,3 +1,41 @@
-function ContextProvider() {}
+function ContextProvider() {
+  const [expenses, setExpenses] = [
+    {
+      id: 1,
+      text: "T-shirt",
+      amount: 20,
+      category: "clothes",
+      date: "01/03/2026",
+    },
+    {
+      id: 2,
+      text: "Chicken",
+      amount: 5,
+      category: "groceries",
+      date: "01/03/2026",
+    },
+    {
+      id: 3,
+      text: "Pizza",
+      amount: 15,
+      category: "eating out",
+      date: "01/03/2026",
+    },
+    {
+      id: 3,
+      text: "Bills",
+      amount: 40,
+      category: "Water bill",
+      date: "01/03/2026",
+    },
+  ];
+
+  const [newExpense, setNewExpense] = {
+    text: "",
+    amount: "",
+    category: "",
+    date: "",
+  };
+}
 
 export default ContextProvider;
